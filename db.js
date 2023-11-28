7cbe687e34eca0b9c2389f17b2bc22b51c392e60
mehran feriduni <m3hr4nus@gmail.com>
2023-11-28 15:16:34 +0330

    read data in database

--- db.js
+++ db.js
@@ -3,8 +3,14 @@ const sql=require('mysql2')
 const pool=sql.createPool({
   host: 'localhost',
   user: 'root',
+  password:'M3hr4nus',
   database: 'mysql',
-})
+}).promise();
 
 
-module.exports={pool}
+const getCurser=async()=>{
+  const rusalt=await pool.query('SELECT * FROM mehran.curses;')
+  console.log(rusalt);
+}
+
+getCurser();
