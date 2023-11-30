25db518dd36b0a46705ca7269af87d5d0dc8bb8b
mehran feriduni <m3hr4nus@gmail.com>
2023-11-30 11:30:40 +0330

    add update delete and insert function

---
+++ middelwares/db.js
@@ -0,0 +1,44 @@
+const sql=require('mysql2')
+require('dotenv').config();
+
+const pool=sql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password:process.env.PASSWORD,
+  database: process.env.DATABASE,
+}).promise();
+
+
+const getCursers=async()=>{
+  const [rusalt]=await pool.query('SELECT * FROM mehran.curses;')
+  return rusalt;
+}
+
+const getCurser=async(id)=>{
+  const [rusalt]=await pool.query
+  ('SELECT * FROM mehran.curses WHERE ID=?  ',[id])
+  return rusalt[0];
+}
+
+const insertCurser=async (Title)=>{
+  const [rusalt]=await pool.query(`INSERT INTO mehran.curses (Title) VALUES (?)`,[Title]);
+  return getCurser(rusalt.insertId);
+};
+
+const updateCurser=async (id,Tilte)=>{
+  const [rusalt]=await pool.query(`UPDATE mehran.curses SET Title=? WHERE id=?`,[Tilte,id]);
+  return getCurser(id);
+}
+
+const deleteCurser=async (id)=>{
+  await pool.query(`DELETE FROM mehran.curses WHERE id=?`,[id]);
+  return id;
+}
+
+getCursers().then((data)=>{
+  console.log(data);
+})
+// getCurser(2).then((data)=>console.log(data));
+// insertCurser('go').then((data)=>console.log(data));
+// updateCurser(8,'NodeJS').then((data)=>console.log(data));
+// deleteCurser(9).then((data)=>console.log(data));
